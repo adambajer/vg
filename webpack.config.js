f3fb6e563740d8197d194d75bfcacf1c2ac05fdb
adambajer <adam.bajer@gmail.com>
2024-05-24 07:46:12 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js', // Adjust this path based on your project structure
+  entry: './index.html', // Adjust this path based on your project structure
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
